d9cbcb3c5609688de68912193fdb52496df047fe
Duane Murphy <dmurphy@vandenrobotics.com>
2018-02-03 14:23:53 -0800

    Update motion profile for Mjolnir
    This version is for autorun.

--- src/main/java/Tank.java
+++ src/main/java/Tank.java
@@ -9,11 +9,20 @@ import jaci.pathfinder.modifiers.TankModifier;
 
 public class Tank {
 
-    public static final double SAMPLE_INTERVAL_SECONDS = 0.05;
-    public static final double MAX_VELOCITY = 1.7;    // units must be consistant feet/second?
-    public static final double MAX_ACCELERATION = 2.0; // units must be consistent feet/second/second?
-    public static final double MAX_JERK = 60.0;       // units must be consistent acceleration/second
-    public static final String NAMESPACE = "CenterGearTrajectories";
+    // Settings for Mjolnir
+    // Distances are in Feet
+    // Velocity is in feet/sec
+    // Accelleration is in feet/sec/sec
+    // Jerk is in feet/sec/sec/sec
+
+    public static final double INCHES_PER_FOOT = 12;
+    public static final double SECONDS_PER_MINUTE = 60;
+    public static final double SAMPLE_INTERVAL_SECONDS = 0.01;
+    public static final double ROBOT_MAX_VELOCITY_FPS = 13.5;
+    public static final double MAX_VELOCITY_FPS = ROBOT_MAX_VELOCITY_FPS * 0.75;     // units must be consistant feet/second?
+    public static final double MAX_ACCELERATION_FPSPS = MAX_VELOCITY_FPS * 1.0; // units must be consistent feet/second/second?
+    public static final double MAX_JERK_FPSPSPS = 60.0;        // units must be consistent acceleration/second
+    public static final String NAMESPACE = "AutoRunTrajectories";
 
 
     public static void main(String[] args) {
@@ -22,35 +31,29 @@ public class Tank {
               Trajectory.FitMethod.HERMITE_QUINTIC, // HERMITE_CUBIC
               Trajectory.Config.SAMPLES_HIGH,
               SAMPLE_INTERVAL_SECONDS,
-              MAX_VELOCITY,
-              MAX_ACCELERATION,
-              MAX_JERK);
+              MAX_VELOCITY_FPS,
+              MAX_ACCELERATION_FPSPS,
+              MAX_JERK_FPSPSPS);
 
-        // Waypoints are x, y, heading (in radians) where x is the direction in front of the robot
-        Waypoint[] rightGearPoints = new Waypoint[] {
-                new Waypoint(0, 0, 0),
-                // new Waypoint(6, 0, Pathfinder.d2r(60)),
-                new Waypoint(8, 2.5, Pathfinder.d2r(60))
-        };
-
-
-        // Base line distance = 7'9 3/4"
-        final double BASE_LINE_DISTANCE_FEET = 7.0 + (9.75 / 12);
+        final double AUTO_LINE_DISTANCE_INCHES = 120.0;
+        final double AUTO_LINE_DISTANCE_FEET = AUTO_LINE_DISTANCE_INCHES / INCHES_PER_FOOT;
         // Robot length = 35 1/2"
-        final double ROBOT_LENGTH_FEET = 35.5 / 12;
-        final double GEAR_DRIVE_DISTANCE_FEET = BASE_LINE_DISTANCE_FEET - (ROBOT_LENGTH_FEET / 2.0);
+        final double BUMPER_THICKNESS_INCHES = 0.75 + 2.5;
+        final double ROBOT_LENGTH_FEET = (32.5 + (BUMPER_THICKNESS_INCHES * 2)) / INCHES_PER_FOOT;
+        // Put the robot half way across the line.
+        final double AUTO_RUN_DISTANCE_FEET = AUTO_LINE_DISTANCE_FEET - (ROBOT_LENGTH_FEET / 2.0);
 
         Waypoint[] gearPoints = new Waypoint[] {
                 new Waypoint(0, 0, 0),
-                new Waypoint(GEAR_DRIVE_DISTANCE_FEET, 0, 0)
+                new Waypoint(AUTO_RUN_DISTANCE_FEET, 0, 0)
         };
 
         // Waypoint[] points = rightGearPoints;
         Waypoint[] points = gearPoints;
         Trajectory trajectory = Pathfinder.generate(points, config);
 
-        final double WHEEL_BASE_INCHES = 30.0;
-        final double WHEEL_BASE_FEET = WHEEL_BASE_INCHES / 12.0;
+        final double WHEEL_BASE_INCHES = 27.5;
+        final double WHEEL_BASE_FEET = WHEEL_BASE_INCHES / INCHES_PER_FOOT;
         TankModifier modifier = new TankModifier(trajectory).modify(WHEEL_BASE_FEET);
 
         // Do something with the new Trajectories...
@@ -69,13 +72,13 @@ public class Tank {
     }
 
     public static final double WHEEL_DIAMETER_INCHES = 4.0;
-    public static final double WHEEL_DIAMETER_FEET = WHEEL_DIAMETER_INCHES / 12.0;  // 1/3
-    public static final double FEET_TO_ROTATION = 1.0 / (WHEEL_DIAMETER_FEET * Math.PI);  // 0.9549
-    public static final double FPS_TO_RPM = FEET_TO_ROTATION * 60;  // 57.9
+    public static final double WHEEL_DIAMETER_FEET = WHEEL_DIAMETER_INCHES / INCHES_PER_FOOT;  // 1/3
+    public static final double FEET_PER_ROTATION = 1.0 / (WHEEL_DIAMETER_FEET * Math.PI);  // 0.9549
+    public static final double FPS_TO_RPM = FEET_PER_ROTATION * SECONDS_PER_MINUTE;  // 57.9
 
     public static void WriteToStruct(File out, Trajectory leftTrajectory, Trajectory rightTrajectory)
     {
-      WriteToStruct(out, leftTrajectory.segments, rightTrajectory.segments, FEET_TO_ROTATION, FPS_TO_RPM);
+      WriteToStruct(out, leftTrajectory.segments, rightTrajectory.segments, FEET_PER_ROTATION, FPS_TO_RPM);
     }
 
     public static void WriteToStruct(File file,
@@ -88,16 +91,16 @@ public class Tank {
       try {
         PrintWriter out = new PrintWriter(file);
 
-        out.printf("#include \"ChassisMotionProfileCommand.h\"\n");
+        out.printf("#include \"MotionProfile.h\"\n");
         out.printf("namespace %s\n{\n\n", NAMESPACE);
 
         // Default linkage is extern for non-const and static for const.
         // Change the linkage for const by specifing extern.
         out.printf("extern const unsigned int kTrajectoryLength = %d;\n\n", leftSegments.length);
-        out.println("extern const ChassisMotionProfileCommand::TrajectoryPoint leftTrajectories[] = {");
+        out.println("extern const robovikes::TrajectoryPoint leftTrajectories[] = {");
         PrintSegments(out, leftSegments, positionScale, velocityScale );
 
-        out.println("extern const ChassisMotionProfileCommand::TrajectoryPoint rightTrajectories[] = {");
+        out.println("extern const robovikes::TrajectoryPoint rightTrajectories[] = {");
         PrintSegments(out, rightSegments, positionScale, velocityScale);
         out.printf("\n} // namespace %s\n", NAMESPACE);
 
